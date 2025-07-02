36794a70d45a52363a7151dad966e90230b7c6fa
fernando Cardoso <fernando.superd@hotmail.com>
2025-07-02 14:42:46 -0300

    Resolvendo exercicios catalogosLivros e orndeacaoPessoa
    Signed-off-by: fernando Cardoso <fernando.superd@hotmail.com>

--- src/main/java/org/example/ordenacaoEmList/ordenacaoDePessoas/OrdenacaoPessoas.java
+++ src/main/java/org/example/ordenacaoEmList/ordenacaoDePessoas/OrdenacaoPessoas.java
@@ -1,14 +1,34 @@
 package org.example.ordenacaoEmList.ordenacaoDePessoas;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoPessoas {
-    public void adicionarPessoa(String nome, int idade, double altura) {
-        //TODO Implementar método
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
     }
 
-    public void ordenarPorIdade() {
+    public void adicionarPessoa(String nome, int idade, double altura) {
         //TODO Implementar método
+        if (!pessoaList.isEmpty()) {
+            pessoaList.add(new Pessoa(nome, idade, altura));
+        }
     }
-    public void ordenarPorAltura() {
+
+    public List<Pessoa> ordenarPorIdade() {
         //TODO Implementar método
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+    public List<Pessoa> ordenarPorAltura() {
+        //TODO Implementar método
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
     }
 }
--- src/main/java/org/example/pesquisaEmLista/catalogoDeLivros/CatalogoLivros.java
+++ src/main/java/org/example/pesquisaEmLista/catalogoDeLivros/CatalogoLivros.java
@@ -1,18 +1,55 @@
 package org.example.pesquisaEmLista.catalogoDeLivros;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CatalogoLivros {
-    public void adicionarLivro(String nome, String autor, int anoPublicacao) {
-        //TODO Implementar método
+    private List<Livro> livrosList;
+
+    public CatalogoLivros() {
+        this.livrosList = new ArrayList<>();
     }
 
-    public void pesquisarPorAutor(String autor) {
-        //TODO Implementar método
-    }
-    public void pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
-        //TODO Implementar método
+    public void adicionarLivros(String titulo, String autor, int anoPublicacao) {
+        livrosList.add(new Livro(titulo, autor, anoPublicacao));
     }
 
-    public void pesquisarPorTitulo(String titulo) {
-        //TODO Implementar método
+    public List<Livro> pesquisaPorAutor(String autor) {
+        List<Livro> listLivrosPorAutor = new ArrayList<>();
+        if (!livrosList.isEmpty()) {
+            for (Livro livro : livrosList) {
+                if (livro.getAutor().equalsIgnoreCase(autor)) {
+                    listLivrosPorAutor.add(livro);
+                }
+            }
+        }
+        return listLivrosPorAutor;
     }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> listaLivrosPorAno = new ArrayList<>();
+        if (!livrosList.isEmpty()){
+            for (Livro livro : livrosList) {
+                if (livro.getAnoPublicacao() >= anoInicial && livro.getAnoPublicacao() <= anoFinal){
+                    livrosList.add(livro);
+                }
+            }
+        }
+        return  listaLivrosPorAno;
+    }
+
+    public Livro pesquisarPorTitulo (String titulo) {
+        Livro livroPorTitulo = null;
+        if (!livrosList.isEmpty()) {
+            for (Livro livro : livrosList) {
+                if (livro.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = livro;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+
 }
